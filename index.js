a96b9492d69f4cfd06498d356ca18caff9939997
Sondos Ahmed <sondosalshami@gmail.com>
2020-08-10 18:12:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,15 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet(){
-
+function greet(time){
+timeArray = time.split(':');
+if (timeArray[0]<12){
+  return "Good Morning";
+}
+else if (timeArray[0]<=17 && timeArray[0]>=12)
+return "Good Afternoon";
+else 
+return "Good Evening";
 }
 /* Write your implementation of displayMessage() */
 function displayMessage(){
